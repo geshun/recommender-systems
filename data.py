641e42055056f55df06072b61c329e7e53a15dae
geshun <gideoneshun@gmail.com>
2024-08-05 08:30:19 -0700

    random interactions data

--- data.py
+++ data.py
@@ -7,17 +7,25 @@ np.random.seed(42)
 # Generate sample data
 n_employees = 500
 n_resources = 100
-n_interactions = 1000
+n_interactions = 4000
 
-# generate user data
+# Generate employee data
 employees = pd.DataFrame({
     'employee_id': np.arange(1, n_employees + 1),
-    'job_role': np.random.choice(['Engineer', 'Manager', 'Analyst'], n_employees),
+    'job_role': np.random.choice([
+        'Software Development Engineer',
+        'Product Manager',
+        'Data Scientist',
+        'Operations Manager',
+        'HR Specialist',
+        'Financial Analyst',
+        'Marketing Specialist'
+    ], n_employees),
     'past_activities': np.random.randint(0, 21, n_employees),
     'feedback_score': np.random.randint(1, 6, n_employees)
 })
 
-# generate item data
+# Generate item data
 resources = pd.DataFrame({
     'resource_id': np.arange(1, n_resources + 1),
     'resource_type': np.random.choice(['Video', 'Article', 'Course'], n_resources),
@@ -25,11 +33,11 @@ resources = pd.DataFrame({
     'popularity_score': np.random.randint(1, 101, n_resources)
 })
 
-# generate interactions data
+# Generate interactions data
 interactions = pd.DataFrame({
     'employee_id': np.random.choice(employees['employee_id'], n_interactions, replace=True),
     'resource_id': np.random.choice(resources['resource_id'], n_interactions, replace=True),
-    'interaction_score': np.random.randint(1, 6, n_interactions)
+    'interaction_score': np.random.randint(1, 5, n_interactions)
 })
 
 print(employees.head())
